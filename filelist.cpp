81b6efc5ceaaf8ce5dfdc356611939c3fa393765
AdolfAlcantara <raulalcantara_96@hotmail.com>
2017-02-11 17:15:56 -0600

    addicion de setters y getters de atributis de filelist y constructor

--- filelist.cpp
+++ filelist.cpp
@@ -1,6 +1,21 @@
 #include "filelist.h"
 
-FileList::FileList()
+FileList::FileList(QString path)
 {
-
+    node *Nodo1 = new Node(path);
+    setHead(Nodo1);
+    setTail(Nodo1);
+    setCant(1);
+}
+void FileList::setHead(node *nodo){
+    head = nodo;
+}
+void FileList::setTail(node * nodo){
+    tail = nodo;
+}
+void FileList::setCant(int value){
+    cant = value;
+}
+int FileList::getCant(){
+    return cant;
 }
